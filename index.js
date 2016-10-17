2b3b22e2747f5d33ca7f3b0ca4a76c31a6e8279b
KHA HOANG Y NGUYEN <ykha@cs.washington.edu>
2016-10-17 16:12:18 -0700

    update server side

--- index.js
+++ index.js
@@ -43,128 +43,114 @@ FB.api('oauth/access_token', {
     console.log(accessToken);
 });
 
-
-
-
-
-/*
-var FacebookTokenStrategy = require('passport-facebook-token');
-
-passport.use(new FacebookTokenStrategy({
-        clientID: 785828631519992,
-        clientSecret: "5e8bd30a6c03fd70d1231fbc67bedb37"
-    }, function(accessToken, refreshToken, profile, done) {
-        User.findOrCreate({facebookId: profile.id}, function (error, user) {
-            return done(error, user);
-        });
-    }
-));
-*/
-
-FB.setAccessToken("EAACEdEose0cBAP5AFrBKHiLeSPBVIc0dJD3uoHNoYRvMkC4x0HfqK81EZAisZBFXLVyvew9K3fUOEg5mtDWxRodiFyIVVJilW3v13y4e6EG05NI94rrpJVYGPByWitIlW1Fg1QlopEzHm2RbNuqHILfs0b9HvmiKPIDCeMmgZDZD");
-FB.api(
-    '/me/posts',
-    'GET',
-    {},
-    function(response) {
-        // Insert your code here
-        //console.log(response["data"]);
-        data = response["data"];
-        var result = "";
-        for (var i = 0; i < data.length; i++) {
-            if (typeof data[i]["message"] !== "undefined") {
-                result += data[i]["message"];
+app.get('/mood', function(req, res) {
+    FB.setAccessToken("EAACEdEose0cBABL9EPi0dpvHhDwropUjmhMe9Jxxpmc6zyEeMjPWjz0kaJfG8TI3ePWhBHyDFSRWghi35XEmhgN8geZCFoiVbT1zu8ho2FvuovZBKTH1d4ZCtHrvW3JwEishVxZCRXZAQclrUqTAhAZB4R8xf3JTr2fpU3oxCKrwZDZD");
+    FB.api(
+        '/me/posts',
+        'GET',
+        {},
+        function(response) {
+            // Insert your code here
+            //console.log(response["data"]);
+            data = response["data"];
+            var result = "";
+            for (var i = 0; i < data.length; i++) {
+                if (typeof data[i]["message"] !== "undefined") {
+                    result += data[i]["message"];
+                }
             }
-        }
 
-        //console.log(result);
-        fetch('https://api.uclassify.com/v1/uclassify/sentiment/Classify?readkey=xQSx6uk7KsVm&text=' + result)
-            .then(function(res) {
-                return res.json();
-            }).then(function(json) {
-            console.log(json);
-        });
-    }
-);
-
-
-
-
-FB.api(
-    '/me/photos',
-    'GET',
-    {"fields":"images"},
-    function(response) {
-        // Insert your code here
-        var dict_array = [];
-        //console.log(response["data"][0]);
-
-        for (var i = 0; i < response["data"].length && i < 7; i++) {
-            var source = response["data"][i]["images"][0]["source"];
-            var count = 0;
-            var max = Math.min(response["data"].length, 7);
-            var dict = {};
-            var p1 = new Promise(
-                function(resolve, reject) {
-                // predict the contents of an image by passing in a url
-                app_clarify.models.predict(Clarifai.GENERAL_MODEL, source).then(
-                    function(response) {
-                        var concepts = response["data"]["outputs"][0]["data"]["concepts"];
-
-                        count++;
-                        for(var i = 0; i < concepts.length; i++) {
-                            var current = concepts[i].name;
-                            if (typeof dict[current] === "undefined") {
-                                dict[current] = 1;
-                            } else {
-                                dict[current] = dict[current] + 1;
-                            }
-                        }
-                        resolve(count);
-                        //console.log(dict_array);
-
-                    },
-                    function(err) {
-                        console.error(err);
-                        reject(err);
-                    }
-                );
+            //console.log(result);
+            fetch('https://api.uclassify.com/v1/uclassify/sentiment/Classify?readkey=xQSx6uk7KsVm&text=' + result)
+                .then(function(res) {
+                    return res.json();
+                }).then(function(json) {
+                console.log(json);
             });
-            p1.then(
-                function(val) {
-                    if (count == max) {
-                        var sorted = [];
-                        for(var key in dict) {
-                            sorted[sorted.length] = key;
+        }
+    );
+
+
+
+
+    FB.api(
+        '/me/photos',
+        'GET',
+        {"fields":"images"},
+        function(response) {
+            // Insert your code here
+            var dict_array = [];
+            //console.log(response["data"][0]);
+
+            for (var i = 0; i < response["data"].length && i < 7; i++) {
+                var source = response["data"][i]["images"][0]["source"];
+                var count = 0;
+                var max = Math.min(response["data"].length, 7);
+                var dict = {};
+                var p1 = new Promise(
+                    function(resolve, reject) {
+                        // predict the contents of an image by passing in a url
+                        app_clarify.models.predict(Clarifai.GENERAL_MODEL, source).then(
+                            function(response) {
+                                var concepts = response["data"]["outputs"][0]["data"]["concepts"];
+
+                                count++;
+                                for(var i = 0; i < concepts.length; i++) {
+                                    var current = concepts[i].name;
+                                    if (typeof dict[current] === "undefined") {
+                                        dict[current] = 1;
+                                    } else {
+                                        dict[current] = dict[current] + 1;
+                                    }
+                                }
+                                resolve(count);
+                                //console.log(dict_array);
+
+                            },
+                            function(err) {
+                                console.error(err);
+                                reject(err);
+                            }
+                        );
+                    });
+                p1.then(
+                    function(val) {
+                        if (count == max) {
+                            var sorted = [];
+                            for(var key in dict) {
+                                sorted[sorted.length] = key;
+                            }
+                            sorted.sort();
+                            // console.log(sorted);
+                            result = "";
+                            for (var i = 0; i < sorted.length && i < 5; i++) {
+                                result += sorted[i];
+                            }
+                            //console.log(result);
+                            fetch('https://api.uclassify.com/v1/uclassify/sentiment/Classify?readkey=xQSx6uk7KsVm&text=' + result)
+                                .then(function(res) {
+                                    return res.json();
+                                }).then(function(json) {
+                                console.log(json);
+                            });
                         }
-                        sorted.sort();
-                        //console.log(sorted);
-                        result = "";
-                        for (var i = 0; i < sorted.length && i < 5; i++) {
-                            result += sorted[i];
-                        }
-                        //console.log(result);
-                        fetch('https://api.uclassify.com/v1/uclassify/sentiment/Classify?readkey=xQSx6uk7KsVm&text=' + result)
-                            .then(function(res) {
-                                return res.json();
-                            }).then(function(json) {
-                            console.log(json);
-                        });
-                    }
 
 
 
 
-                }).catch(
-                function(reason) {
-                    console.log('Handle rejected promise ('+reason+') here.');
-                });
+                    }).catch(
+                    function(reason) {
+                        console.log('Handle rejected promise ('+reason+') here.');
+                    });
 
-        };
+            };
 
 
-    }
-);
+        }
+    );
+});
+
+
 
 
 app.listen(app.get('port'), function(){
